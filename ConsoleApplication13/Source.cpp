1d57f5f4086869e0bdbb5189c6ad0d15eeff3a4c
dibrova <dibroff@pochta.ru>
2017-12-19 20:37:08 +0200

    first commit

---
+++ ConsoleApplication13/Source.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+#define LOG cout << __FUNCTION__ << endl;
+
+class MyClassA
+{
+public:
+	MyClassA() { LOG };
+	virtual ~MyClassA() { LOG };
+
+private:
+
+	
+};
+
+class MyClassB : public MyClassA
+{
+public:
+	MyClassB() { LOG };
+	~MyClassB() { LOG };
+
+private:
+
+};
+
+
+
+bool check(char* str) {
+	stack <char> st;
+	for (size_t i = 0; i < strlen(str); i++)
+	{
+		if (str[i] == '(')
+		{
+			st.push(str[i]);
+		}
+		if (str[i] == ')' && st.size() && st.top() == '(')
+		{
+			st.pop();
+		}
+		else if (str[i] == ')')
+		{
+			st.push(str[i]);
+		}
+	}
+	if (st.empty())
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void main() {
+	char a;
+	cout << check("(1+2*(8-1)*(3-5)+(8-2))");
+	cout << endl;
+	cout << check(")(1+2*)(((8-1)*(3-5)+(8-2)+)(");
+	cout << endl;
+	cin >> a;
+
+}
